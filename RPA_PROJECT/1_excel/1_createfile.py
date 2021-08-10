a7140d81007f0eadc1fac6874eaea9c4d0e1a9b9
lastsky20 <lastsky20@naver.com>
2021-08-10 22:08:45 +0900

    210810-1

--- RPA_PROJECT/1_excel/1_createfile.py
+++ RPA_PROJECT/1_excel/1_createfile.py
@@ -3,7 +3,6 @@ from openpyxl import Workbook   # 엑셀 관련 라이브러리
 wb = Workbook()  # 새 워크북 생성
 ws = wb.active  # 활성화된 시트 선택
 ws.title = "TestSheet1"  # 타이틀 변경
-
 wb.save("text.xlsx")  # 파일 저장
 wb.close()  # 파일 닫기
 
--- Webscrapping_basic/3_requests.py
+++ Webscrapping_basic/3_requests.py
@@ -4,6 +4,8 @@ res = requests.get("http://www.naver.com")
 res.raise_for_status()      # 예외처리
 res2 = requests.get("http://www.tistory.com")
 res2.raise_for_status()     # 예외처리
+res3 = requests.get("http://www.google.co.kr")
+res3.raise_for_status()
 
 
 print("응답코드 :", res.status_code)
@@ -16,5 +18,8 @@ print("응답코드 :", res2.status_code)
 #     print("Error Code : {0}".format(res.status_code))
 
 res.raise_for_status()  # 예외처리
+
+with open("myGoogle.html", "w", encoding = "utf8") as f:
+    f.write(res3.text)
 
     
