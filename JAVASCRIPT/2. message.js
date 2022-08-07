45d0a3e4a6d68ef1c6b9d90d000ac92c514e36f5
lastsky20 <lastsky20@naver.com>
2022-08-07 12:09:12 +0900

    220807-1

---
+++ JAVASCRIPT/2. message.js
@@ -0,0 +1,9 @@
+//alert  알려줌
+//prompt 입력받음
+//confirm 확인받음
+
+// const name1 = prompt("이름을 입력하세요"); // 메시지와 입력필드, 취소 시 null, 두번째 인수는 디폴트 지정 가능
+// alert(`안녕하세요,  님 환영합니다.`);  // 메시지와 확인버튼
+// const isAdult = console.log("당신은 성인 입니까?")  // true, false 반환
+
+// 
