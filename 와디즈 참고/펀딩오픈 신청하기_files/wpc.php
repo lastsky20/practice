027cb04db0d224a14471f95bfcce070c61c77ad5
lastsky20 <lastsky20@naver.com>
2023-01-23 13:04:28 +0900

    230123-1

---
+++ 와디즈 참고/펀딩오픈 신청하기_files/wpc.php
@@ -0,0 +1,78 @@
+/*<![CDATA[*/
+(function(w) {
+    var wg = w.document.getElementById('wp_tg_cts');
+    function doPair(url) {
+        if (wg == null) { return; }
+        (function(_url) {
+            var frm = w.document.createElement('IFRAME');
+            frm.width = '1px';
+            frm.height = '1px';
+            frm.style.display = 'none';
+            frm.src='about:blank';
+            frm.title = 'tgpairing';
+            wg.appendChild(frm);
+
+            var ifrm = (frm.contentWindow) ? frm.contentWindow : (frm.contentDocument.document ? frm.contentDocument.document : frm.contentDocument);
+            ifrm.document.open();
+            ifrm.document.write('<img src=\"' + _url + '\"/>');
+            ifrm.document.close();
+
+            setTimeout(function() {
+                wg.removeChild(frm);
+            }, 2000);
+        })(url);
+    }
+
+    try {
+        var links = [],
+            len = links.length,
+            i;
+        for (i=0; i<len; i++) {
+            doPair(links[i]);
+        }
+    } catch(e) {}
+})(window);
+/*]]>*/
+
+
+
+
+
+
+/*<![CDATA[*/
+(function(w) {
+    var origin = "https:\/\/astg.widerplanet.com";
+    var wg = w.document.getElementById('wp_tg_cts');
+    function doPair(url) {
+        if (wg == null) { return; }
+        (function(_url) {
+            var frm = w.document.createElement('IFRAME');
+            frm.width = '1px';
+            frm.height = '1px';
+            frm.style.display = 'none';
+            frm.src= _url;
+            frm.title = 'tgpairing';
+            frm.addEventListener('load', function(o) {
+                try {
+                    frm.contentWindow.postMessage({}, origin);
+                } catch(e) {}
+            });
+
+            wg.appendChild(frm);
+            setTimeout(function() {
+                wg.removeChild(frm);
+            }, 3000);
+        })(url);
+    }
+
+    try {
+        doPair("https:\/\/astg.widerplanet.com\/delivery\/storage?request_id=85870387978dff1c3ab80f62728d1d43\u0026wp_uid=2-139033226a6ad5ee83fd89ccb73d048c-s1674379797.254000%7Cwindows_10%7Cchrome-jyw3oe\u0026qsc=1y5kxax");
+    } catch(e) {}
+})(window);
+/*]]>*/
+
+
+
+
+
+
